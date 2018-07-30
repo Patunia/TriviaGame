aee90fcb4694fe31568aa3cc70c390f3991c3962
patunia <pborowiec@alumni.nd.edu>
2018-07-29 19:10:27 -0500

    more style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-// simple timer set to 50 seconds
-setTime(timeUp, 1000 * 50);
+// // simple timer set to 50 seconds
+// setTime(timeUp, 1000 * 50);
 
-//console.log time up
-function timeUp() {
-    console.log("done");
-    $("#time-left").append("<p>Your time is up!</p>");
-    console.log(" the time is up");
-}
+// //console.log time up
+// function timeUp() {
+//     console.log("done");
+//     $("#time-left").append("<p>Your time is up!</p>");
+//     console.log(" the time is up");
+// }
