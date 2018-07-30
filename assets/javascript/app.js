558e51992684ec8652401d70a330df6ddfb52977
patunia <pborowiec@alumni.nd.edu>
2018-07-29 20:57:00 -0500

    js and css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,35 @@
-function startTimer(){
-    var timer = 50;
-    setInterval(function() {
-        timer--;
-      if (timer >= 0) {
-        span = document.getElementById("count");
-        span.innerHTML = timer;
-      }
-      if (timer === 0) {
-          alert('time is up!');
-          clearInterval(timer);
-      }
-    }, 1000);
-  }
-  $("#startClock").click(function(){
-      startTimer();
-   });
+$(document).ready(function(){
+    function startTimer(){
+        var timer = 50;
+        setInterval(function() {
+            timer--;
+        if (timer >= 0) {
+            span = document.getElementById("count");
+            span.innerHTML = timer;
+        }
+        if (timer === 0) {
+            alert('time is up!');
+            clearInterval(timer);
+        }
+        }, 1000);
+    }
+
+    $("#startClock").click(function(){
+        startTimer();
+    });
+
+    function finish(){
+        number = 1;
+        clearInterval(timer);
+    }
+
+    $("#submit").on("click", finish);
+
+    function restart(){
+        number = 50;
+        start();
+    }
+
+    $("#restart").on("click", restart);
+
+});
